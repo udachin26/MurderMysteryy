ebb25e1481ccd87cd124d9ab0c45f18414a43574
ItsToxicGG <95104544+ItsToxicGG@users.noreply.github.com>
2022-02-24 22:22:46 +0200

    Update mm.php

--- murdermystery/toxicgg/mm.php
+++ murdermystery/toxicgg/mm.php
@@ -1,1 +1,25 @@
 <?php
+
+namespace murdermystery\toxicgg;
+
+use pocketmine\plugin\PluginBase;
+use pocketmine\player\Player;
+
+use pocketmine\entity\Entity;
+use pocketmine\entity\EntityFactory;
+
+class mm extends PluginBase{
+  
+  public function onLoad{
+    $this->getLogger->info("Loading MurderMystery...");
+    $this->getLogger->info("...");
+  }
+  
+  public function onEnable{
+    $this->getLogger->info("Enabled MurderMystery");
+    $this->getLogger->info("By ItsToxicGG");
+  }
+  
+  public function onDisable{
+    $this->getLogger->info("Disabling MurderMystery !!!");
+  }
