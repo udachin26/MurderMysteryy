176a212b2e4566e174c1acd702b0d40ea7439607
ItsToxicGG <95104544+ItsToxicGG@users.noreply.github.com>
2022-03-09 18:17:49 +0200

    Update Commands.php

--- murdermystery/toxicgg/commands/Commands.php
+++ murdermystery/toxicgg/commands/Commands.php
@@ -6,25 +6,5 @@ use pocketmine\command\Command;
 use pocketmine\command\CommandSender;
 
 class Commands extends CommandSender{
-  
-  public function onCommand(CommandSender $sender, Command $command, string $label, array $args) : bool{
-    switch($command->getName()){
-        case "help":
-            $sender->sendMessage("§c-======= MurderMystery Commands =======-");
-            $sender->sendMessage("§c/murdermystery help");
-            $sender->sendMessage("§c/murdermystery info");
-            $sender->sendMessage("§c/murdermystery setspawns");
-            $sender->sendMessage("§c/murdermystery setlobby");
-            $sender->sendMessage("§c/murdermystery create");
-            $sender->sendMessage("§c/murdermystery random");
-
-            return true;
-        case "info":
-           $sender->sendMessage("§eMurderMystery Plugin by ItsToxicGG");
-           $sender->sendMessage("§cFound an problem? Report a issue on github or contact me on discord (ItsToxicGG#7420)");
-        
-           return true;
-        default:
-            throw new \AssertionError("This line will never be executed");
-    }
+   // code will come out!
 }
