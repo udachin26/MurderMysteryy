977eaee85621f6c2549fd60611d1ca0d929b33f3
ItsToxicGG <95104544+ItsToxicGG@users.noreply.github.com>
2022-03-09 18:05:42 +0200

    soon entity

---
+++ murdermystery/toxicgg/utils/SEntity.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace murdermystery\toxicgg\utils;
+
+use pocketmine\player\Player;
+use pocketmine\entity\Entity;
+use pocketmine\entity\EntityFactory;
+
+class SEntity extends Entity{
+  // Soon
+}
