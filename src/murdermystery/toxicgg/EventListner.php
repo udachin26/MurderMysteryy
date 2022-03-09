6ccb96718b64df46dacfc50b9735e81e189245f7
ItsToxicGG <95104544+ItsToxicGG@users.noreply.github.com>
2022-03-09 18:56:09 +0200

    who else say teeeeeeesssssst

--- src/murdermystery/toxicgg/EventListner.php
+++ src/murdermystery/toxicgg/EventListner.php
@@ -14,6 +14,62 @@ use pocketmine\math\Vector3;
 use pocketmine\player\Player;
 use pocketmine\utils\TextFormat;
 
+use murdermystery\toxicgg\Murder;
+use murdermystery\toxicgg\Arena\Arena;
+
 class EventListener implements Listener{
-   // soon like normal but have alot planned
-}
+   
+    public function onQuit(PlayerQuitEvent $event){
+        $player = $event->getPlayer();
+        if(($game = Murder::getInstance()->getPlayerGame($player)) instanceof Arena){
+            $game->removePlayer($player);
+        }
+    }
+
+    public function onPlace(BlockPlaceEvent $event){
+        $player = $event->getPlayer();
+        if(($game = Murder::getInstance()->getPlayerGame($player)) instanceof Game){
+            $game->placedblock[Utils::vectorToString($event->getBlock()->getPosition()->asVector3())] = $event->getBlock()->getPosition()->asVector3();
+        }
+    }
+
+    /**
+     * @param BlockBreakEvent $event
+     */
+    public function onBreak(BlockBreakEvent $event){
+        $player = $event->getPlayer();
+        if(($game = Murder::getInstance()->getPlayerGame($player)) instanceof Game){
+            if(isset($game->placedblock[Utils::vectorToString($event->getBlock()->getPosition()->asVector3())])){
+                unset($game->placedblock[Utils::vectorToString($event->getBlock()->getPosition()->asVector3())]);
+            } else {
+                $event->cancel();
+            }
+        }
+    }
+   
+    public function onExhaust(PlayerExhaustEvent $event){
+        $player = $event->getPlayer();
+        if(($game = Murder::getInstance()->getPlayerGame($player)) instanceof Game){
+            $event->cancel();
+            $event->getPlayer()->getHungerManager()->setFood(20);
+        }
+    }
+
+    public function onDamage(EntityDamageEvent $event){
+        $player = $event->getEntity();
+        if($player instanceof Player) {
+            if (($game = Murder::getInstance()->getPlayerGame($player)) instanceof Game) {
+                if($game->phase == "LOBBY" || $game->phase == "COUNTDOWN"){
+                    $event->cancel();
+                }
+            }
+        }
+    }
+
+    public function onChat(PlayerChatEvent $event){
+        $player = $event->getPlayer();
+        if(($game = TheBridge::getInstance()->getPlayerGame($player)) instanceof Game) {
+            $game->broadcastMessage($player, $event->getMessage());
+            $event->cancel();
+        }
+    }
