9ee1ff76d48778ca7b20ea4ba048e7da8df66cc8
ItsToxicGG <95104544+ItsToxicGG@users.noreply.github.com>
2022-03-09 18:47:37 +0200

    Update EventListner.php

--- src/murdermystery/toxicgg/EventListner.php
+++ src/murdermystery/toxicgg/EventListner.php
@@ -2,6 +2,18 @@
 
 namespace murdermystery\toxicgg;
 
-class EventListner extends Events{
-   // soon
+use pocketmine\event\block\BlockBreakEvent;
+use pocketmine\event\block\BlockPlaceEvent;
+use pocketmine\event\entity\EntityDamageEvent;
+use pocketmine\event\Listener;
+use pocketmine\event\player\PlayerChatEvent;
+use pocketmine\event\player\PlayerExhaustEvent;
+use pocketmine\event\player\PlayerMoveEvent;
+use pocketmine\event\player\PlayerQuitEvent;
+use pocketmine\math\Vector3;
+use pocketmine\player\Player;
+use pocketmine\utils\TextFormat;
+
+class EventListener implements Listener{
+   // soon like normal but have alot planned
 }
