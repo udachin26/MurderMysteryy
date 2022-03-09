00156693bef48a6bb3e5b0ad06ef39f1e19a2bb6
ItsToxicGG <95104544+ItsToxicGG@users.noreply.github.com>
2022-03-09 18:27:58 +0200

    scoreboard aint ready

--- src/murdermystery/toxicgg/Arena/arena.php
+++ src/murdermystery/toxicgg/Arena/arena.php
@@ -16,19 +16,5 @@ class arena extends Arena{
     /** @var int */
     private int $timer = 300; //5 minutes
   
-    public function tick(): void
-    {
-         switch ($this->phase) {
-             case "LOBBY":
-                 foreach ($this->players as $player) {
-                     if ($player->isOnline()) {
-                         ScoreFactory::setObjective($player, TextFormat::YELLOW . TextFormat::BOLD . "MURDER MYSTER");
-                         ScoreFactory::setScoreLine($player, 1, TextFormat::YELLOW . "Players: ". TextFormat::GREEN . count($this->players) . "/16");
-                         ScoreFactory::setScoreLine($player, 2, TextFormat::YELLOW . "play.yourservername.com");
-                         ScoreFactory::sendObjective($player);
-                         ScoreFactory::sendLines($player);
-                     }
-                 }
-         }
-    }
+   }
                        
